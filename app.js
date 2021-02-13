23e2a8a09ef41dd86dd3e99a3fd81c5ef2317091
jet08082000 <jet08082000@gmail.com>
2021-02-14 06:06:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! First git code'));
+app.get('/', (req, res) => res.send('Hello World! First git code. Finish AWS beantalk deploymenet '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
